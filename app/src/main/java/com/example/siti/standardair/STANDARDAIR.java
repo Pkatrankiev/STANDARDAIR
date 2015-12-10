43412ec661b60ce08ac3cb0d83fa89cbb5d3c929
Pkatrankiev <pkatrankiev@abv.bg>
2015-12-10 13:01:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/siti/standardair/STANDARDAIR.java
@@ -0,0 +1,13 @@
+package com.example.siti.standardair;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class STANDARDAIR extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_standardair);
+    }
+}
